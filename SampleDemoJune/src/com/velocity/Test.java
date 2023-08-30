4bd41e4d02618a21ae9a0fc75623155842cab08e
Jeevan <javaindustryexperience@gmail.com>
2023-08-30 09:12:50 +0530

    arraylist code added here

---
+++ SampleDemoJune/src/com/velocity/Test.java
@@ -0,0 +1,15 @@
+package com.velocity;
+
+import java.util.ArrayList;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		ArrayList al = new ArrayList();
+		al.add(20);
+		al.add(50);
+		al.add(70);
+		System.out.println(al);
+	}
+}
